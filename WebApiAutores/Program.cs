cced638d782dc4568373cfaa81a715cff361514c
Juliants10 <juliants10@gmail.com>
2023-01-12 20:39:24 -0300

    comment removed

--- WebApiAutores/Program.cs
+++ WebApiAutores/Program.cs
@@ -7,7 +7,7 @@ var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
-//
+
 startup.Configure(app,app.Environment);
 // Configure the HTTP request pipeline.
 app.Run();
