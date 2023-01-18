00c6309b5b1895a09a70461aff6f8a714aecbad1
JulianTormes <juliants10@gmail.com>
2023-01-18 10:18:50 -0300

    Leyendo registros con EF Core

--- WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -28,32 +28,29 @@ namespace WebApiAutores.Controllers
         [HttpGet]//api/autores (hereda ruta)
 
         //(Multiples rutas)
-        public async Task<ActionResult<List<Autor>>> Get()
+        public async Task<ActionResult<List<AutorDTO>>> Get()
         {
-            return await _context.Autores.ToListAsync();
+            var autores = await _context.Autores.ToListAsync();
+            return _mapper.Map<List<AutorDTO>>(autores);
         }
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<Autor>> Get(int id)
+        public async Task<ActionResult<AutorDTO>> Get(int id)
         {
-            var autor = await _context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            var autor = await _context.Autores.FirstOrDefaultAsync(autorBD => autorBD.Id == id);
 
             if (autor == null)
             {
                 return NotFound();
             }
-            return autor;
+            return _mapper.Map<AutorDTO>(autor);
 
         }
         [HttpGet("{nombre}")]
-        public async Task<ActionResult<Autor>> Get([FromRoute] string nombre)
+        public async Task<ActionResult<List<AutorDTO>>> Get([FromRoute] string nombre)
         {
-            var autor = await _context.Autores.FirstOrDefaultAsync(x => x.Nombre.Contains(nombre));
+            var autores = await _context.Autores.Where(autorBD => autorBD.Nombre.Contains(nombre)).ToListAsync();
 
-            if (autor == null)
-            {
-                return NotFound();
-            }
-            return autor;
+            return _mapper.Map<List<AutorDTO>>(autores);
 
         }
 
@@ -87,7 +84,7 @@ namespace WebApiAutores.Controllers
                 return BadRequest("El id del autor no coincide con el id de la URL");
             }
 
-            var existe = await _context.Autores.AnyAsync(x => x.Id == id);
+            var existe = await _context.Autores.AnyAsync(autorBD => autorBD.Id == id);
             if (!existe)
             {
                 return NotFound();
