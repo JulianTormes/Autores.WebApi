e4983e49e9a3cae860efa589488bfa26fef2bfee
JulianTormes <juliants10@gmail.com>
2023-01-24 14:46:08 -0300

    Mas pruebas unitarias

--- WebAPIAutores.Test/PruebasUnitarias/PrimeraLetraMayusculaAttributeTests.cs
+++ WebAPIAutores.Test/PruebasUnitarias/PrimeraLetraMayusculaAttributeTests.cs
@@ -21,5 +21,31 @@ namespace WebAPIAutores.Test.PruebasUnitarias
             //Verificacion
             Assert.AreEqual("La primera letra debe ser mayuscula", resultado.ErrorMessage);
         }
+        [TestMethod]
+        public void ValorNulo_NoDevuelveError ()
+        {
+            //Preparacion
+            var primeraLetraMayuscula = new PrimeraLetraMayusculaAttribute();
+            string valor = null;
+            var valContext = new System.ComponentModel.DataAnnotations.ValidationContext(new { Nombre = valor });
+
+            //Ejecuccion
+            var resultado = primeraLetraMayuscula.GetValidationResult(valor, valContext);
+            //Verificacion
+            Assert.IsNull(resultado);
+        }
+        [TestMethod]
+        public void ValorConPrimerLetraMayuscula_NoDevuelveError()
+        {
+            //Preparacion
+            var primeraLetraMayuscula = new PrimeraLetraMayusculaAttribute();
+            string valor = "Julian";
+            var valContext = new System.ComponentModel.DataAnnotations.ValidationContext(new { Nombre = valor });
+
+            //Ejecuccion
+            var resultado = primeraLetraMayuscula.GetValidationResult(valor, valContext);
+            //Verificacion
+            Assert.IsNull(resultado);
+        }
     }
 }
